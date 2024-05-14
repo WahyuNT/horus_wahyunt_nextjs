fae67efcececcdffecd9b308f5284ccba4d4e413
Wahyu NT <wahyuntx28@gmail.com>
2024-05-15 02:01:09 +0700

    chore: Add link to history page in navbar

--- pages/history.tsx
+++ pages/history.tsx
@@ -16,8 +16,8 @@ export default function Home() {
           </a>
           Halaman Voucher
           <Link href={"/history"}>
-
-            <button className="btn btn-info rounded-pill px-3">History</button>
+          
+          <button className="btn btn-info rounded-pill px-3">History</button>
           </Link>
         </div>
       </nav>
@@ -28,10 +28,41 @@ export default function Home() {
       <div className="container">
 
         <div className="d-flex justify-content-between mt-4">
+          <div className="col-9">
+            <div className="d-flex">
+              <Link href={"/"}>
+
+                <button className="btn btn-sm btn-warning rounded-pill"><i className="fa-solid fa-arrow-left"></i></button>
+              </Link>
+              <h4 className="mb-0 fw-bold ms-2">List Voucher</h4>
+            </div>
+            <div className="card mx-0 me-3 ">
+              <div className="div">
+                <table className="table table-bordered ">
+                  <thead>
+                    <tr>
+                      <th scope="col">#</th>
+                      <th scope="col">Voucher</th>
+                      <th scope="col">Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th >1</th>
+                      <td>Mark</td>
+                      <td><button className="btn btn-danger btn-sm  rounded-pill">Delete</button></td>
+                    </tr>
+
+
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
           <div className="col-3 p-0 ">
             <div className="card sidebar shadow-none m-0 d-flex flex-column align-items-between  mb-3">
 
-              <h5 className="text-center fw-bold">Kategori Voucher</h5>
+              <h5 className="text-center fw-bold">Voucher Terklaim</h5>
               <div className="div">
                 <a href="/" className="text-decoration-none ">
                   <div className="card card-sidebar-active shadow-none px-1 py-3 my-2 ">
@@ -82,34 +113,7 @@ export default function Home() {
 
             </div>
           </div>
-          <div className="col-9 ps-2">
-            <h4 className="ps-2 mb-0 fw-bold">List Voucher</h4>
-            <div className="row flex-wrap">
 
-              <div className="col-4 p-0 m-0">
-                <div className="card ">
-                  <img className="img-voucher" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ1VuKA1bfF-J9EICmf9n4YvfTkXkhQb4Zln2kVXHZnw&s" alt="" />
-
-                  <div className="d-flex justify-content-between mt-3 align-items-center">
-                    <div className="col d-flex flex-column">
-
-                      <span className="m-0 p-0 fw-bold">Diskon 50</span>
-                      <small className="p-0 m-0">
-                        <small>
-                          Fashion
-                        </small>
-                      </small>
-                    </div>
-                    <div className="col text-end">
-                      <button className="btn btn-sm btn-success rounded-pill px-2">Claim</button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-
-            </div>
-          </div>
         </div>
       </div>
     </>
