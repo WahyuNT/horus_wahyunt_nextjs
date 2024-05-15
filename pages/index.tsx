cea901f2d8379686d57378b3e711ca0b0576bd28
Wahyu NT <wahyuntx28@gmail.com>
2024-05-15 18:35:04 +0700

    chore: Add claim functionality for vouchers and improve code readability

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,11 +9,32 @@ import checkAuth from "./middleware/authMiddleware";
 import { NextPageContext } from 'next';
 import axios from "axios";
 import Cookies from 'js-cookie';
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
   const token = Cookies.get('jwt');
+  const Claim = async (voucherId: string) => {
+    try {
+      const response = await fetch(`http://127.0.0.1:8000/api/voucher/${voucherId}/claim`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        fetchData();
+      } else {
+        // Tangani kasus respons tidak berhasil jika diperlukan
+      }
+    } catch (error) {
+      console.error('Terjadi kesalahan:', error);
+    }
+  };
   const fetchData = async () => {
     try {
       const responseVoucher = await axios.get('http://127.0.0.1:8000/api/get-voucher', {
@@ -33,6 +54,7 @@ export default function Home() {
   useEffect(() => {
     checkAuth({} as NextPageContext);
     fetchData();
+
   }, []);
 
 
@@ -65,30 +87,24 @@ export default function Home() {
           <div className="col-12 col-lg-9 ps-2">
             <h4 className="ps-2 mb-0 fw-bold">List Voucher</h4>
             <div className="row flex-wrap">
-              {voucher.map((item: { _id: string, foto: string, nama: string, kategori: string, status: string }) => {
-                return (
-                  <div className="col-12 col-lg-4 p-0 m-0">
-                    <div className="card ">
-                      <img className="img-voucher" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ1VuKA1bfF-J9EICmf9n4YvfTkXkhQb4Zln2kVXHZnw&s" alt="" />
-
-                      <div className="d-flex justify-content-between mt-3 align-items-center">
-                        <div className="col-9 d-flex flex-column">
-
-                          <span className="m-0 p-0 fw-bold">{item.nama}</span>
-                          <small className="p-0 m-0">
-                            <small>
-                            {item.kategori}
-                            </small>
-                          </small>
-                        </div>
-                        <div className="col-3 text-end">
-                          <button className="btn btn-sm btn-success rounded-pill px-2">Claim</button>
-                        </div>
+              {voucher.map((item: { id: string, foto: string, nama: string, kategori: string, status: string }) => (
+                <div className="col-12 col-lg-4 p-0 m-0" key={item.id}>
+                  <div className="card">
+                    <img className="img-voucher" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ1VuKA1bfF-J9EICmf9n4YvfTkXkhQb4Zln2kVXHZnw&s" alt="" />
+                    <div className="d-flex justify-content-between mt-3 align-items-center">
+                      <div className="col-9 d-flex flex-column">
+                        <span className="m-0 p-0 fw-bold">{item.nama}</span>
+                        <small className="p-0 m-0">
+                          <small>{item.kategori}</small>
+                        </small>
+                      </div>
+                      <div className="col-3 text-end">
+                        <button onClick={() => Claim(item.id)} className="btn btn-sm btn-success rounded-pill px-2">Claim</button>
                       </div>
                     </div>
                   </div>
-                )
-              })}
+                </div>
+              ))}
 
             </div>
           </div>
