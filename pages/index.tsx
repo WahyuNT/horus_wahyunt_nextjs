061eb00db11d38640929ecf9b6d436e2cd091eef
Wahyu NT <wahyuntx28@gmail.com>
2024-05-16 00:47:58 +0700

    chore: Update image rendering in history and home pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -86,14 +86,22 @@ export default function Home() {
   return (
     <>
       <nav className="navbar navbar-light bg-light">
-        <div className="container">
+        <div className="container d-flex align-items-center">
           <a className="navbar-brand" href="#">
-            Voucher
+            <div className=" d-none d-lg-block">
+              Voucer App
+            </div>
+            <div className="d-lg-none d-block">
+              <button className="btn btn-bar d-flex align-items-centeer" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <i className="fa-solid fa-bars fa-xl"></i>
+              </button>
+            </div>
           </a>
-          <h4 className="fw-bold">
-            Halaman Voucher
+          <h4 className="fw-bold d-none d-lg-block">
+            Beranda
           </h4>
           <Link href={"/history"}>
+
             <button className="btn btn-info rounded-pill px-3">History</button>
           </Link>
         </div>
@@ -102,6 +110,41 @@ export default function Home() {
 
 
 
+      <div className="modal fade" id="exampleModal" tabIndex={-1} aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog ">
+          <div className="modal-content p-3">
+            <div className="card sidebar-mobile shadow-none border-0 m-0 d-flex flex-column align-items-between  mb-3">
+
+              <h5 className="text-center fw-bold">Kategori Voucher</h5>
+              {kategori.map((item: { id: string, kategori: string, jumlah: string }) => (
+                <div className="" onClick={() => handleFilterChange(item.kategori)}>
+                  <div className={`card my-2 shadow-none px-1 py-3 ${valueFilter === item.kategori ? 'card-sidebar-active' : 'card-sidebar'}`}>
+                    <div className="d-flex justify-content-between px-3">
+                      <div className="div">
+                        {item.kategori}
+                      </div>
+                      <div className="div">
+                        {item.jumlah}
+                      </div>
+                    </div>
+                  </div>
+
+
+                </div>
+              ))}
+              <div className="d-flex justify-content-center">
+
+                <button className="btn btn-sm btn-warning btn-sm rounded-pill px-4" onClick={() => handleFilterChange('')}>Reset Filter</button>
+              </div>
+              <div className="d-flex justify-content-center mt-auto ">
+                <button onClick={handleLogout} className="btn btn-danger rounded-pill w-50 ">Log Out</button>
+              </div>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="container">
 
         <div className="d-flex justify-content-between mt-4">
